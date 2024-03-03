2d9b40db2c751a4e61a729f836086af531d51c67
ArtemOn456 <artemon4xxl@gmail.com>
2024-03-03 12:00:32 +0300

    Мультиплеер и новые функции

--- dev/button.ts
+++ dev/button.ts
@@ -5,17 +5,19 @@ function setButton() {
       | ItemContainer
       | com.zhekasmirnov.innercore.api.mod.ui.container.UiAbstractContainer
   ) {
-    const players = Network.getConnectedPlayers();
-    for(const i in players){
-    const client = Network.getClientForPlayer(players[i]);
-    client.send("hl.manipulate", {})
-    }
+    
+      Network.sendToServer("hl.manipulate", {});
+    
   };
+
+  const COORDS_Y = __config__.getFloat("button_y");
+  const COORDS_X = __config__.getFloat("button_x");
+  const BUTTON_SIZE = __config__.getFloat("button_size");
 
   const BUTTON = new UI.Window({
     location: {
-      x: 5,
-      y: UI.getScreenHeight() / 2 - 110,
+      x: COORDS_X,
+      y: UI.getScreenHeight() / 2 - COORDS_Y,
       width: 100,
       height: 100,
     },
@@ -33,7 +35,8 @@ function setButton() {
         x: 0,
         y: 0,
         bitmap: "button",
-        scale: 30,
+        bitmap2: "button_2",
+        scale: BUTTON_SIZE,
         clicker: {
           onClick: function (position, container) {
             return click(position, container);
@@ -59,3 +62,59 @@ Callback.addCallback("NativeGuiChanged", function (screenName) {
   }
 });
 
+const Left = {
+  getItem: function (entity, id, data?) {
+    const item = Entity.getOffhandItem(entity);
+    if (item.id == id && (item.data == data || 0)) return true;
+    return false;
+  },
+  itemPlacer(
+    block: number,
+    item: { id: number; count: number; data: number } | any,
+    entity,
+    coords: Callback.ItemUseCoordinates
+  ) {
+    const rel = coords.relative;
+    const block_ = BlockSource.getDefaultForActor(entity);
+    const item_ = Entity.getCarriedItem(entity);
+    return (
+      Entity.setCarriedItem(
+        entity,
+        item,
+        item_.count - 1,
+        0,
+        null
+      ),
+      block_.setBlock(rel.x, rel.y, rel.z, block, 0),
+      Game.message("???")
+    );
+  },
+  itemReplacer(item1, item2, entity, coords, block1, block2?) {
+    if (Left.getItem(entity, item1, 0)) {
+     return ( Left.itemPlacer(block1, item1, entity, coords),
+      Entity.setOffhandItem(
+        entity,
+        item2,
+        1,
+        0,
+        null
+      ) )
+    } else if (Left.getItem(entity, item2, 0)) {
+     return ( Left.itemPlacer(block2 || 0, item2, entity, coords),
+      Entity.setOffhandItem(
+        entity,
+        item1,
+        1,
+        0,
+        null
+      ) )
+    }
+  },
+};
+
+ModAPI.registerAPI("HandLeftAPI", {
+  Left: Left,
+  requireGlobal: function (command) {
+    return eval(command);
+  },
+});
